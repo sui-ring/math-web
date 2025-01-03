3505b6c7e8e0e573297e6059cf91bc07424a177c
go-numb <gonumb66@gmail.com>
2025-01-03 21:36:29 +0900

    sitemap date: past date

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -17,7 +17,8 @@ interface SitemapRow {
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const now = new Date()
+    // const now = new Date()
+    const now = new Date('2024-12-31T00:00:00Z')
 
     // ベースURLを取得
     const baseUrl = getBaseUrl()
